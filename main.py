eedc53b93fee226cf79d3700390f5bf275e09ac3
Hamza-theGOAT <hamza.b0302@gmail.com>
2025-07-30 02:40:16 +0500

    Comparison with existing data and improved output saving func

--- main.py
+++ main.py
@@ -5,44 +5,81 @@ import json
 
 
 def repoClean(dp: dict):
-    df = dp['amzOrd'].copy()
+    df = dp['amzOrd_Exp'].copy()
     prod = dp['prod'].copy()
-    df = df[df['Sales Channel'] == 'Amazon.com']
+
+    # Filter DF for Sales Channel and entries with valid amount
+    df = df[(df['Sales Channel'] == 'Amazon.com') &
+            (df['Product/Service Amount'].notna())]
+
+    # Get Product/Service Name against SKUs
     df = pd.merge(df, prod[['Product/Service Name',
                   'SKU']], on='SKU', how='left')
     df['Product/Service Name'] = df['Product/Service Name'].replace(
         np.nan, '[Error Term]')
+
+    # Calculate Item rate from Item Amount and Quantity
     df['Product/Service Rate'] = df['Product/Service Amount'] / \
         df['Product/Service Quantity']
 
-    df = df[dp['colz']['ordWorkColz']]
+    # Filter for Orders in Order Report posted in system
+    df['Check'] = df['Sales Receipt No'].isin(
+        dp['sysRcrds'][dp['data']['sysfltr']]
+    )
+    df = df[df['Check'] == False]
+
+    # Filter out unnecessary columns from the DF
+    df = df[dp['data']['ordWorkColz']]
+
+    # Include the filtered DF in dp dict for later use
+    dp['Working_Exp'] = df.copy()
+
     print(f'Adjusted DataFrame:\n{df}')
+    return dp
 
 
-def depen(src):
+def toExcel(dp: dict):
+    # Create the output workbook path
+    wbN = os.path.join(dp['data']['wrk'], dp['data']['wbN'])
+
+    # ExcelWriter loop for saving all dp values with '_Exp' in key
+    with pd.ExcelWriter(wbN) as writer:
+        for key, value in dp.items():
+            if key.endswith('_Exp'):
+                sh = key.replace('_Exp', '')
+                value.to_excel(writer, sheet_name=sh, index=False)
+
+
+def depen():
     with open('variables.json', 'r') as j:
-        colz = json.load(j)
+        data = json.load(j)
 
-    ordRepo = os.path.join(src, 'amazonOrdersReport.txt')
+    ordRepo = os.path.join(data['src'], data['ordRepo'])
     amzOrd = pd.read_csv(
-        ordRepo, delimiter='\t', encoding='utf-16', usecols=colz['ordRepoColz'].keys()
-    ).rename(columns=colz['ordRepoColz'])
+        ordRepo, delimiter='\t', encoding='utf-16', usecols=data['ordRepoColz'].keys()
+    ).rename(columns=data['ordRepoColz'])
 
-    prod = pd.read_excel(os.path.join(src, 'productManual.xlsx'))
+    sysRcrds = pd.read_excel(
+        os.path.join(data['src'], data['sysRcrds']),
+        header=3
+    ).dropna(subset=data['sysfltr'])
+    prod = pd.read_excel(os.path.join(data['src'], data['prod']))
+
     print(f'Amazon Order Report:\n{amzOrd}')
 
     return {
-        "colz": colz,
-        'amzOrd': amzOrd,
+        "data": data,
+        'amzOrd_Exp': amzOrd,
+        'sysRcrds': sysRcrds,
         'prod': prod
     }
 
 
 def main():
     # Get dependencies for the script
-    src = 'source_dir'
-    dp = depen(src)
-    repoClean(dp)
+    dp = depen()
+    dp = repoClean(dp)
+    toExcel(dp)
 
 
 if __name__ == '__main__':
